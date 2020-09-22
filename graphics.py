abd38398991aeee2988a2684b374ef95dd1b6388
WirKekc <bk9419@yandex.ru>
2020-09-22 22:25:17 +0300

    del some #

--- graphics.py
+++ graphics.py
@@ -1,35 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import win32com.client
 from calculation import t1_y, t2_y, t1_2sk_y, t1_apr_y, t2_apr_y, t2_2_y, q_y, q_apr_y, m1_y, m2_y, q_s_y,\
     q_s_apr_y, ud_nagrev_y, temperature, t_nv, m1, m2, date
-
-#
-# Excel = win32com.client.Dispatch("Excel.Application")
-# wb = Excel.Workbooks.Open(u'D:\\Downloads\\Графики_Угданская_7.xlsx')
-# sheet = wb.Worksheets(u'Лист1')
-
-# X значения
-# x = np.array([r[0].value for r in sheet.Range(f"I3:I143")])
-# x1 = np.array([r[0].value for r in sheet.Range(f"U3:U60")])
-# x_date = np.array([r[0].value for r in sheet.Range(f"K3:K143")])
-
-# Y значения
-# t1_y = np.array([r[0].value for r in sheet.Range(f"B3:B143")])
-# t2_y = np.array([r[0].value for r in sheet.Range(f"C3:C143")])
-# t1_2sk_y = np.array([r[0].value for r in sheet.Range(f"W3:W60")])
-# t1_apr_y = np.array([r[0].value for r in sheet.Range(f"Y3:Y60")])
-# t2_apr_y = np.array([r[0].value for r in sheet.Range(f"Z3:Z60")])
-# t2_2_y = np.array([r[0].value for r in sheet.Range(f"X3:X60")])
-# q_y = np.array([r[0].value for r in sheet.Range(f"H3:H143")])
-# q_apr_y = np.array([r[0].value for r in sheet.Range(f"AC3:AC60")])
-# m1_y = np.array([r[0].value for r in sheet.Range(f"E3:E143")])
-# m2_y = np.array([r[0].value for r in sheet.Range(f"F3:F143")])
-# q_s_y = np.array([r[0].value for r in sheet.Range(f"P3:P143")])
-# q_s_apr_y = np.array([r[0].value for r in sheet.Range("AD3:AD60")])
-# ud_nagr_y = np.array([r[0].value for r in sheet.Range("O3:O143")])
-# m1_date_y = np.array([r[0].value for r in sheet.Range("L3:L143")])
-# m2_date_y = np.array([r[0].value for r in sheet.Range("M3:M143")])
 
 
 fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(
@@ -106,4 +77,3 @@ ax5.set_xlabel('Дата')
 ax5.set_ylabel('Масса, т')
 
 plt.show()
-
